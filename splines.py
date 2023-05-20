38a85b67906da0abf540d2bc20d0d3e84b3e2aa1
psycake <52770775+psycake@users.noreply.github.com>
2023-05-20 15:06:48 -0600

    n now is the number of interval (instead of points)

--- splines.py
+++ splines.py
@@ -8,6 +8,7 @@ class SplinePolynomial:
 	simplified_form = sympify(0)
 
 class Splines:
+	# n es el número de intervalos; n+1 puntos
 	n = int()
 	points = list()
 
@@ -17,29 +18,31 @@ class Splines:
 	s = list() 
 	polynomials = list()
 
+	equations_to_solve = list()
+
 	def __init__(self):
-		self.n = Input.int("¿Cuántos puntos tiene la tabla? ")
+		self.n = Input.int("¿Cuántos intervalos tiene la tabla? ")
 
-		self.points = [[0 for _ in range(2)] for _ in range(self.n)]
+		self.points = [[0 for _ in range(2)] for _ in range(self.n+1)]
 
-		self.h = [[0 for _ in range(2)] for _ in range(self.n-1)]
-		self.differences = [[0 for _ in range(2)] for _ in range(self.n-1)]
+		self.h = [0 for _ in range(self.n)]
+		self.differences = [0 for _ in range(self.n)]
 
-		self.s = [[0 for _ in range(2)] for _ in range(self.n-1)]
-		self.polynomials = [[0 for _ in range(2)] for _ in range(self.n-1)]
+		self.s = [0 for _ in range(self.n)]
+		self.polynomials = [0 for _ in range(self.n)]
+
+		self.equations_to_solve	= [0 for _ in range(self.n)]
 
 		print("Para introducir los puntos en la tabla, siga el formato x,f(x):")
-		for i in range(self.n):
+		for i in range(self.n+1):
 			self.points[i] = Input.real(f"• Punto {i}: ", 2)
 
-		for i in range(self.n-1):
+		for i in range(self.n):
 			self.h[i] = self.points[i+1][x] - self.points[i][x]
 			self.differences[i] = (self.points[i+1][y] - self.points[i][y])/(self.points[i+1][x] - self.points[i][x])
 
-		self.s[0], self.s[self.n] = 0, 0
-
-		# print(self.points)
-		# print(self.h)
-		# print(self.differences)
+		print(self.points)
+		print(self.h)
+		print(self.differences)
 
 test = Splines()
